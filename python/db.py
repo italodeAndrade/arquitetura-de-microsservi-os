8e12813af62c2afe0c30b8f0004401b0fcfb1874
Ítalo de Andrade <italolande@gmail.com>
2025-09-04 19:52:15 -0300

    login python

--- python/db.py
+++ python/db.py
@@ -7,10 +7,23 @@ def get_db_connection():
         conn = mysql.connector.connect(
             host=os.getenv('DB_HOST', 'localhost'),
             user=os.getenv('DB_USER', 'root'),
-            password=os.getenv('DB_PASS', ''),
-            database=os.getenv('DB_NAME', '')
+            password=os.getenv('DB_PASS', 'Simba8!#'),
+            database=os.getenv('DB_NAME', 'rouppa')
         )
         return conn
     except mysql.connector.Error as err:
         print(f"Erro ao conectar ao banco de dados: {err}")
         return None
+
+
+def logar_usuario(email,senha):
+    conn = get_db_connection()
+    if conn:
+        cursor = conn.cursor()
+        query = "SELECT * FROM usuario WHERE email = %s AND senha = %s"
+        cursor.execute(query, (email, senha))
+        usuario = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return usuario
+    return None
--- python/main.py
+++ python/main.py
@@ -1,8 +1,30 @@
 from flask import Flask
+from flask import Flask, request, jsonify
+from db import init_db
 
 app = Flask(__name__)
 
 @app.route('/logar', methods=['POST'])
 def logar():
-    query = 'select * from usuarios where email = :email and senha = :senha'
-    
+    dados = request.get_json()
+
+
+    if not dados or not dados.get('email') or not dados.get('senha'):
+        return jsonify({"erro": "Email e senha são obrigatórios"}), 400
+
+    email = dados.get('email')
+    senha = dados.get('senha')
+
+    usuario = logar_usuario(email, senha)
+
+
+    if usuario:
+
+        return jsonify({"message": "Login realizado com sucesso!"})
+    else:
+
+        return jsonify({"erro": "Email ou senha inválidos"}), 401
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
